829f30ae721bbe0460dcccbdf667b95d52288acf
devadigapratham <devadigapratham8@gmail.com>
2024-11-30 22:10:00 +0530

    basic stuff

---
+++ build-server/script.js
@@ -0,0 +1,50 @@
+const { exec } = require('child_process')
+const fs = require('fs')
+const path = require('path')
+const {S3Client, PutObjectCommand} = require('@aws-sdk/client-s3')
+const mime = require('mime-types')
+
+const s3Client = new S3Client ({
+    region: '',
+    credentials: {
+        accessKeyId: '',
+        secretAccessKey: ''
+    }
+})
+
+const PROJECT_ID = process.env.PROJECT_ID
+
+async function init() {
+    console.log('Executing script.js')
+    const outDirPath = path.join(__dirname, 'output') 
+
+    const p = exec(`cd ${outDirPath} && npm install && npm run build`)
+
+    p.stdout.on('data', function (data){
+        console.log(data.toString())
+    })
+
+    p.stdout.on('error', function (data) {
+        console.log('Error', data.toString())
+    })
+
+    p.on('close', async function() {
+        console.log('Build Complete')
+        const distFolderPath = path.join(__dirname, 'output', 'dist')
+        const distFolderContents = fs.readdirSync(distFolderPath, {recursive: true})
+
+        for (const filePath of distFolderContents) {
+            if (fs.lstat.Sync(filePath).isDirectory()) continue; 
+
+            const command = new PutObjectCommand({
+                Bucket: '',
+                Key: `__outputs/${PROJECT_ID}/${filePath}`, 
+                Body: fs.createReadStream(filePath),
+                ContentType: mime.lookup(filePath)
+            })
+
+            await s3Client.send(command)
+        }
+        console.log('Done!')
+    })
+}
